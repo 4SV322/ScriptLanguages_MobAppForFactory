e661003c83793022cfe218ccf6d9edcc5d154f5f
?? <91481987+4SV322@users.noreply.github.com>
2023-12-11 18:04:52 +0800

    version 1.1.1.1.1.1.1.1.1.1.1.1.1.1111
    Very simple idk which data need to be added or edited, can edit it rn

---
+++ add_data.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+def add_data(account_number, fio, address, ipu_info, comments):
+    conn = sqlite3.connect('ves_database.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        INSERT INTO ves_data (account_number, fio, address, ipu_info, comments)
+        VALUES (?, ?, ?, ?, ?)
+    ''', (account_number, fio, address, ipu_info, comments))
+
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    # Пример добавления данных
+    add_data('12345', 'Иванов Иван', 'ул. Примерная 1', 'ИПУ_Инфо_1', 'Комментарий к данным 1')
+    add_data('67890', 'Петров Петр', 'ул. Тестовая 2', 'ИПУ_Инфо_2', 'Комментарий к данным 2')
---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+
+app = Flask(__name__)
+
+# Создаем базу данных и таблицу
+conn = sqlite3.connect('ves_database.db')
+cursor = conn.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS ves_data (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        account_number TEXT,
+        fio TEXT,
+        address TEXT,
+        ipu_info TEXT,
+        comments TEXT
+    )
+''')
+conn.commit()
+conn.close()
+
+# Определение маршрутов
+@app.route('/')
+def index():
+    # Чтение данных из базы данных
+    conn = sqlite3.connect('ves_database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM ves_data')
+    data = cursor.fetchall()
+    conn.close()
+    return render_template('index.html', data=data)
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    # Получение данных из базы данных для редактирования
+    conn = sqlite3.connect('ves_database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM ves_data WHERE id = ?', (id,))
+    data = cursor.fetchone()
+
+    if request.method == 'POST':
+        # Обновление данных в базе данных
+        new_comments = request.form['comments']
+        cursor.execute('UPDATE ves_data SET comments = ? WHERE id = ?', (new_comments, id))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('index'))
+
+    conn.close()
+    return render_template('edit.html', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
