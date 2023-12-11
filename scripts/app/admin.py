2a22d2c4318b5fd9c46a9f95f86e894dbe734c92
?? <91481987+4SV322@users.noreply.github.com>
2023-12-11 17:35:36 +0800

    edit
    123

--- scripts/app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Account
-
-
-class AccountAdmin(admin.ModelAdmin):
-    list_display = ('account_number', 'owner_name', 'address', 'reading', 'stamp', 'comment')
-
-
-admin.site.register(Account, AccountAdmin)
--- scripts/app/serializers.py
+++ scripts/app/serializers.py
@@ -4,4 +4,4 @@ from .models import Account
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
-        fields = ('account_number', 'owner_name', 'address')
+        fields = ('account_number', 'owner_name', 'address', 'reading', 'stamp', 'comment')
