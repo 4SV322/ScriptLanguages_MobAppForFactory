a10eaa078e0596b14be5489a55ca6e4d595fe6a3
?? <91481987+4SV322@users.noreply.github.com>
2023-12-11 17:55:13 +0800

    rework
    123

--- scripts/app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Account
-
-class AccountSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Account
-        fields = ('account_number', 'owner_name', 'address', 'reading', 'stamp', 'comment')
--- scripts/scripts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from app.views import AccountViewSet
-
-urlpatterns = [
-    path('accounts/', AccountViewSet.as_view({'get': 'list', 'post': 'create'}), name='accounts'),
-    path('accounts/<int:pk>/', AccountViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='account'),
-]
